b8cf91e5de73f3a5de5d37de6ff7a5b6cb6b55ed
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-03-04 16:24:53 +0900

    Show timer commit

--- js/main.js
+++ js/main.js
@@ -7,18 +7,30 @@
   var reset = document.getElementById('reset');
 
   var startTime;
-  var elapsedTime = 0;
+  var elapsedTime = 0;    // 経過時間初期化
+
+  function updateTimerText() {
+    // 135.2秒経過したとして、135200 -> 02:15.200
+    // m = 135200 / 60000 の商
+    // s = 135200 % 60000 / 1000
+    // ms = 135200 % 1000
+    var m = Math.floor(elapsedTime / 60000);    // 小数点以下切捨て
+    var s = Math.floor(elapsedTime % 60000 / 1000);
+    var ms = elapsedTime % 1000;
+    timer.textContent = m + ':' + s + '.' + ms;
+  }
 
   function countUp() {
     setTimeout(function() {
       elapsedTime = Date.now() - startTime;
-      console.log(elapsedTime);
+      // console.log(elapsedTime);
+      updateTimerText();
       countUp();
-    }, 10);
+    }, 10);               // 10ミリ秒後に
   }
 
   start.addEventListener('click', function() {
     startTime = Date.now();
-    countUp();
+    countUp();            //計算実行
   });
 })();
